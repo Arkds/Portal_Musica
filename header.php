7d86234b01167e50f1674bf7f23b2a63229c6188
Arkds <arnolflo23@gmail.com>
2023-05-11 22:26:50 -0500

    subida

---
+++ header.php
@@ -0,0 +1,44 @@
+<nav class=" navbar bg-dark     navbar-expand-lg bg-body-none" data-bs-theme="dark">
+            <div class=" barrita container-fluid">
+                <a class="navbar-brand" href="#">Sort Sound</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse nav" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Albunes</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Acerca de
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Contactanos</a></li>
+                                <li><a class="dropdown-item" href="#">Suport</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Ramdom</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled">Ayuda</a>
+                        </li>
+                        
+                    </ul>
+                    <div class="login">
+                                <a class="link-light" href="login.html">Login / Sing Up</a>
+                    </div>
+                                      
+                   
+                    
+                </div>
+            </div>
+        </nav>
